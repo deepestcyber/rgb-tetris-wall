3208a55edf14f2bbd8438b242f690a10fbea6dbb
merelyrey <merelyrey@gmail.com>
2018-02-18 23:51:27 +0100

    improved raspi to arduino communication

--- raspi_preproc/send_serial.py
+++ raspi_preproc/send_serial.py
@@ -4,7 +4,7 @@ import datetime
 USBPORT = '/dev/ttyACM1' #check correct port first
 NUM_LEDS_H = 12 #16
 NUM_LEDS_V = 24 #24
-FPS = 50
+FPS = 25
 
 s = serial.Serial(USBPORT, 115200) #115200 230400
 time.sleep(5)
@@ -12,7 +12,7 @@ time.sleep(5)
 leds = [[0 for i in range(NUM_LEDS_V)] for j in range(NUM_LEDS_H)]
 counter = 0
 delaycounter = 1
-delay = 10
+delay = 25
 
 while True:
     timestart = datetime.datetime.now()
@@ -22,14 +22,14 @@ while True:
             leds[i][j] = (256/NUM_LEDS_V*(counter+i+j))%256
     if (delaycounter%delay == 0):    
         counter=(counter+1)%NUM_LEDS_V
-    delaycounter += 1
+    delaycounter=(delaycounter+1)%delay
 
     s.write("".join([chr(m) for n in leds for m in n]))
     s.flush()
     
     timefin = datetime.datetime.now()
-    waittime = max(0.0,(1./FPS)-((timefin-timestart).microseconds/1000000))
-    print(waittime)
+    waittime = max(0.0,(1./FPS)-(0.000001*(timefin-timestart).microseconds))
+    print(waittime, (timefin-timestart).microseconds)
     time.sleep(waittime)                        
 
 
