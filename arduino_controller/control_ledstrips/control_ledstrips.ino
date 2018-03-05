5d64582034680a07a4b65344192b7acdb2f43498
merelyrey <merelyrey@gmail.com>
2018-03-05 10:37:23 +0100

    revised arduino: added buttons, tested for full size

--- arduino_controller/control_ledstrips/control_ledstrips.ino
+++ arduino_controller/control_ledstrips/control_ledstrips.ino
@@ -1,69 +1,149 @@
+//Designed for Arduino Mega
 
 #include "FastLED.h"
+#include "elapsedMillis.h"
 
-#define BUTTON_PIN_0 A0
-#define BUTTON_PIN_1 A1
-#define BUTTON_PIN_2 A2
-#define BUTTON_PIN_3 A3
-#define LEDS_PIN_0 2
-#define LEDS_PIN_1 3
-#define LEDS_PIN_2 4
-#define LEDS_PIN_3 5
-#define LEDS_PIN_4 6
-#define LEDS_PIN_5 7
-#define LEDS_PIN_6 8
-#define LEDS_PIN_7 9
-#define LEDS_PIN_8 10
-#define LEDS_PIN_9 11
-#define LEDS_PIN_10 12
-#define LEDS_PIN_11 13
-#define LEDS_PIN_12 14
-#define LEDS_PIN_13 15
-#define LEDS_PIN_14 18
-#define LEDS_PIN_15 19
+#define LEDS_PIN_0 A0
+#define LEDS_PIN_1 A1
+#define LEDS_PIN_2 2
+#define LEDS_PIN_3 3
+#define LEDS_PIN_4 4
+#define LEDS_PIN_5 5
+#define LEDS_PIN_6 6
+#define LEDS_PIN_7 7
+#define LEDS_PIN_8 8
+#define LEDS_PIN_9 9
+#define LEDS_PIN_10 10
+#define LEDS_PIN_11 11
+#define LEDS_PIN_12 12
+#define LEDS_PIN_13 13
+#define LEDS_PIN_14 20
+#define LEDS_PIN_15 21
+#define BUTTON_PIN_0 A7 //Mode switch
+#define BUTTON_PIN_1 A8 //Decrement submode
+#define BUTTON_PIN_2 A9 //Increment submode
+#define BUTTON_PIN_3 A10 //Decrement pattern speed
+#define BUTTON_PIN_4 A11 //Increment pattern speed
+#define BUTTON_PIN_5 A12 //Decrement brightness
+#define BUTTON_PIN_6 A13 //Increment brightness
+#define POTI_PIN_0 A5 //just for testing
 #define MODEL WS2812B //WS2811, WS2812b
-#define NUM_LEDS_H 12 //16
+#define NUM_LEDS_H 16 //16
 #define NUM_LEDS_V 24 //24
-#define NUM_BITS_VSTREAM 6
+#define NUM_BUTTONS 7 //7
+#define BUTTON_WAIT 50 //time (ms) to wait for another buttoncheck
+#define NUM_POTI 1 //just for testing
+#define NUM_BITS_VSTREAM 8 //6
 #define NUM_FPS_VSTREAM 50
 #define WAITTIME_VSTREAM 20 //1000/NUM_FRAMES_VSTREAM
-#define NUM_BYTES_VSTREAM 288 //NUM_LEDS_H*NUM_LEDS_V*NUM_BITS_VSTREAM/8;
+#define NUM_BYTES_VSTREAM 384 //288 //NUM_LEDS_H*NUM_LEDS_V*NUM_BITS_VSTREAM/8;
 
 CRGB leds[NUM_LEDS_H][NUM_LEDS_V];
 
 //modes: 0 = light patterns, 1 = music patterns, 2 = image stream (24bit), 3 = video stream
-int mode = 3;
+int mode = 0;
+int submode [4] = {0, 0, 0, 0};
+int submodeMax [4] = {128, 1, 1, 1}; //Used for all mode switches
+
+int buttonState [NUM_BUTTONS];         // current state of the button
+int lastButtonState [NUM_BUTTONS];     // previous state of the button
+elapsedMillis elapsedTime;
+int waitingTime = 0; 
+
+int pspeed = 2; //[0..4]
+int cspeed = 512;
+int brightness = 2; //[0..4]
+const int valueb [5][4] = {{3,9,27,81},{4,12,36,108},{5,15,45,135},{7,21,63,189},{9,27,81,243}};
 int state = 0;
+
 //TODO: define a good color palette - perhaps like NES?
 extern const CHSV currentPalette [64] = {CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216), CHSV(255,255,8), CHSV(255,255,24), CHSV(255,255,72), CHSV(255,255,216)};
 
 void setup() {
-  // put your setup code here, to run once:
+  for(int i=0; i<NUM_BUTTONS; i++){
+    buttonState[i] = 0;
+    lastButtonState[i] = 0; 
+  }
+  pinMode(BUTTON_PIN_0, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_1, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_2, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_3, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_4, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_5, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_6, INPUT_PULLUP);
 
   Serial.begin(115200); //115200 230400
 
-  FastLED.addLeds<MODEL, LEDS_PIN_0, RGB>(leds[0], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_1, RGB>(leds[1], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_2, RGB>(leds[2], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_3, RGB>(leds[3], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_4, RGB>(leds[4], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_5, RGB>(leds[5], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_6, RGB>(leds[6], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_7, RGB>(leds[7], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_8, RGB>(leds[8], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_9, RGB>(leds[9], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_10, RGB>(leds[10], NUM_LEDS_V);
-  FastLED.addLeds<MODEL, LEDS_PIN_11, RGB>(leds[11], NUM_LEDS_V);
-  //FastLED.addLeds<MODEL, LEDS_PIN_12, RGB>(leds[12], NUM_LEDS_V);
-  //FastLED.addLeds<MODEL, LEDS_PIN_13, RGB>(leds[13], NUM_LEDS_V);
-  //FastLED.addLeds<MODEL, LEDS_PIN_14, RGB>(leds[14], NUM_LEDS_V);
-  //FastLED.addLeds<MODEL, LEDS_PIN_15, RGB>(leds[15], NUM_LEDS_V);
+  if (NUM_LEDS_H> 0) FastLED.addLeds<MODEL, LEDS_PIN_0, RGB>(leds[0], NUM_LEDS_V);
+  if (NUM_LEDS_H> 1) FastLED.addLeds<MODEL, LEDS_PIN_1, RGB>(leds[1], NUM_LEDS_V);
+  if (NUM_LEDS_H> 2) FastLED.addLeds<MODEL, LEDS_PIN_2, RGB>(leds[2], NUM_LEDS_V);
+  if (NUM_LEDS_H> 3) FastLED.addLeds<MODEL, LEDS_PIN_3, RGB>(leds[3], NUM_LEDS_V);
+  if (NUM_LEDS_H> 4) FastLED.addLeds<MODEL, LEDS_PIN_4, RGB>(leds[4], NUM_LEDS_V);
+  if (NUM_LEDS_H> 5) FastLED.addLeds<MODEL, LEDS_PIN_5, RGB>(leds[5], NUM_LEDS_V);
+  if (NUM_LEDS_H> 6) FastLED.addLeds<MODEL, LEDS_PIN_6, RGB>(leds[6], NUM_LEDS_V);
+  if (NUM_LEDS_H> 7) FastLED.addLeds<MODEL, LEDS_PIN_7, RGB>(leds[7], NUM_LEDS_V);
+  if (NUM_LEDS_H> 8) FastLED.addLeds<MODEL, LEDS_PIN_8, RGB>(leds[8], NUM_LEDS_V);
+  if (NUM_LEDS_H> 9) FastLED.addLeds<MODEL, LEDS_PIN_9, RGB>(leds[9], NUM_LEDS_V);
+  if (NUM_LEDS_H> 10) FastLED.addLeds<MODEL, LEDS_PIN_10, RGB>(leds[10], NUM_LEDS_V);
+  if (NUM_LEDS_H> 11) FastLED.addLeds<MODEL, LEDS_PIN_11, RGB>(leds[11], NUM_LEDS_V);
+  if (NUM_LEDS_H> 12) FastLED.addLeds<MODEL, LEDS_PIN_12, RGB>(leds[12], NUM_LEDS_V);
+  if (NUM_LEDS_H> 13) FastLED.addLeds<MODEL, LEDS_PIN_13, RGB>(leds[13], NUM_LEDS_V);
+  if (NUM_LEDS_H> 14) FastLED.addLeds<MODEL, LEDS_PIN_14, RGB>(leds[14], NUM_LEDS_V);
+  if (NUM_LEDS_H> 15) FastLED.addLeds<MODEL, LEDS_PIN_15, RGB>(leds[15], NUM_LEDS_V);
 
   //setPaletteNES();
 }
 
+void checkPoti() {
+  cspeed = analogRead(POTI_PIN_0);
+}
+
+void checkButtons() {
+
+  // compare the buttonState to its previous state
+  for(int i=0; i<NUM_BUTTONS; i++){
+    if (i==0) buttonState[i] = digitalRead(BUTTON_PIN_0);
+    else if (i==1) buttonState[i] = digitalRead(BUTTON_PIN_1);
+    else if (i==2) buttonState[i] = digitalRead(BUTTON_PIN_2);
+    else if (i==3) buttonState[i] = digitalRead(BUTTON_PIN_3);
+    else if (i==4) buttonState[i] = digitalRead(BUTTON_PIN_4);
+    else if (i==5) buttonState[i] = digitalRead(BUTTON_PIN_5);
+    else if (i==6) buttonState[i] = digitalRead(BUTTON_PIN_6);
+
+    if (buttonState[i] != lastButtonState[i]) {
+      // if the state has changed, increment the counter
+      if (buttonState[i] == LOW) {
+        // if the current state is LOW then the button went from off to on:
+        if (i==0) mode = (mode+1)%4;
+        else if (i==1) submode[mode] = (submode[mode]-1)%submodeMax[mode];
+        else if (i==2) submode[mode] = (submode[mode]+1)%submodeMax[mode];
+        else if (i==3) pspeed = min(pspeed+1,4);
+        else if (i==4) pspeed = max(pspeed-1,0);
+        else if (i==5) brightness = max(brightness-1,0);
+        else if (i==6) brightness = min(brightness+1,4);
+      } else {
+        // if the current state is HIGH then the button went from on to off:
+    }
+      lastButtonState[i] = buttonState[i];
+      // Delay a little bit to avoid bouncing
+    }
+  }
+}
+
+void timedDelay(int waitingTime){
+  checkButtons();
+  while ((waitingTime - (int)elapsedTime) > BUTTON_WAIT) {
+    delay(BUTTON_WAIT);
+    checkButtons();
+  }
+  delay(max(waitingTime - (int)elapsedTime,0));
+  waitingTime=0; //for safety - TODO remove
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  //checkButtons();
+  //checkPoti();
+  elapsedTime = 0;
 
   //mode - video stream: 50fps a frames with 6 bit/px
   if (mode == 3){
@@ -81,29 +161,82 @@ void loop() {
         leds[i][NUM_LEDS_V-1-j] = CHSV((int)data[i*NUM_LEDS_V+j], 255, 64);
       }
     }
-    FastLED.show();
+    waitingTime = WAITTIME_VSTREAM;
   }
+  
   //mode - image stream: one frames with 24 bit/px (at max every 80ms)
   else if (mode == 2){
     //TODO
   }
+  
   //mode - sound activation (hardcoded) - shows pattern accourding to a microphon signal
   else if (mode == 1){
     //TODO
   }
+  
   //mode - dynamic patterns (hardcoded) - perhaps via several sub-modes
   else { //mode == 0
-    for(int i=0; i<NUM_LEDS_H; i++){
-      for(int j=0; j<NUM_LEDS_V; j++){ 
-        leds[i][NUM_LEDS_V-1-j] = CHSV(256/NUM_LEDS_V*((state+i+j)%NUM_LEDS_V+1)-1, 255, 64);
-      }
+    if (submode[0] == 127){
     }
-    state=(state+1)%NUM_LEDS_V;
-    FastLED.show();
-    delay(1000); 
+    else if (submode[0] == 126){
+      //copy this check for creating a new pattern
+    }
+    else if (submode[0] == 5){
+      for(int i=0; i<NUM_LEDS_H; i++){
+        for(int j=0; j<NUM_LEDS_V; j++){ 
+          leds[i][NUM_LEDS_V-1-j] = CRGB(((brightness)*60+3)%256, ((brightness)*60+3)%256, ((brightness)*60+3)%256);
+        }
+      }
+      waitingTime = 1000;
+    }
+    else if (submode[0] == 4){
+      for(int i=0; i<NUM_LEDS_H; i++){
+        for(int j=0; j<NUM_LEDS_V; j++){ 
+          leds[i][NUM_LEDS_V-1-j] = CHSV(rand()%256, (pspeed*64)%256, (brightness*64-1)%256);
+        }
+      }
+      waitingTime = 1000;
+    }
+    else if (submode[0] == 3){
+      for(int i=0; i<NUM_LEDS_H; i++){
+        for(int j=0; j<NUM_LEDS_V; j++){ 
+          leds[i][NUM_LEDS_V-1-j] = CHSV(((state+(rand()%12-1)+j)-1)%256, 255, valueb[brightness][3]);
+        }
+      }
+      state=(state+rand()%8)%256;
+      waitingTime = pspeed*pspeed*62+8;
+    }
+    else if (submode[0] == 2){
+      for(int i=0; i<NUM_LEDS_H; i++){
+        for(int j=0; j<NUM_LEDS_V; j++){ 
+          leds[i][NUM_LEDS_V-1-j] = CHSV((rand()%12+256/NUM_LEDS_V*(state+(rand()%5-1)+j)-1)%256, 255, valueb[brightness][3]);
+        }
+      }
+      state=(state+1)%NUM_LEDS_V;
+      waitingTime = pspeed*pspeed*62+8;
+    }
+    else if (submode[0] == 1){
+      for(int i=0; i<NUM_LEDS_H; i++){
+        for(int j=0; j<NUM_LEDS_V; j++){ 
+          leds[i][NUM_LEDS_V-1-j] = CHSV((7+256/NUM_LEDS_V*(state+i+j)-1)%256, 255, valueb[brightness][3]);
+        }
+      }
+      state=(state+(-1+(rand()%4)))%NUM_LEDS_V;
+      waitingTime = pspeed*pspeed*62+8;
+    }
+    else { //submode[0] == 0
+      for(int i=0; i<NUM_LEDS_H; i++){
+        for(int j=0; j<NUM_LEDS_V; j++){ 
+          leds[i][NUM_LEDS_V-1-j] = CHSV((7+256/NUM_LEDS_V*(state+i+j)-1)%256, 255, valueb[brightness][3]);
+        }
+      }
+      state=(state+1)%NUM_LEDS_V;
+      waitingTime = pspeed*pspeed*62+8;
+    }
+    //delay(cspeed);
   }
 
-  //delay(WAITTIME);    
+  FastLED.show();
+  timedDelay(waitingTime); 
 }
-
 
