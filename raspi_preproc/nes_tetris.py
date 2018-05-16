873dd1bd84ec9805fb6a3c75d980077f6942a921
rey <merelyrey@gmail.com>
2018-05-16 08:37:04 +0200

    progress tetris

--- raspi_preproc/nes_tetris.py
+++ raspi_preproc/nes_tetris.py
@@ -43,7 +43,7 @@ class Nes_tetris:
 
 
     def is_pix_not_black(self, pix):
-        if (pix[0] or pix[1] or pix[2]) >= 10:
+        if (pix[0] or pix[1] or pix[2]) >= 20:
             return True
         return False
 
@@ -105,6 +105,75 @@ class Nes_tetris:
                 self.img_leds.putpixel((12+x, 12), next_block_col)
 
 
+    def extract_score(self, img):
+        #read
+        score = 0 \
+                + 100000 * self.get_number(img.crop((0, 0, 9, 9))) \
+                + 10000 * self.get_number(img.crop((8, 0, 8 + 9, 9))) \
+                + 1000 * self.get_number(img.crop((16, 0, 16 + 9, 9))) \
+                + 100 * self.get_number(img.crop((24, 0, 24 + 9, 9))) \
+                + 10 * self.get_number(img.crop((32, 0, 32 + 9, 9))) \
+                + self.get_number(img.crop((40, 0, 40 + 9, 9)))
+
+        #write
+
+
+    def extract_level(self, img):
+        #read
+        level = 0 \
+                + 10 * self.get_number(img.crop((0, 0, 9, 9))) \
+                + self.get_number(img.crop((8, 0, 17, 9)))
+        #write
+
+
+    def extract_lines(self, img):
+        #read
+        lines = 0 \
+                + 100 * self.get_number(img.crop((0, 0, 9, 9))) \
+                + 10 * self.get_number(img.crop((8, 0, 17, 9))) \
+                + self.get_number(img.crop((16, 0, 25, 9)))
+        #write
+
+
+    def get_number(self, img):
+        #img.save("debug.png", "PNG")
+        number = 0
+
+        #read
+        if not self.is_pix_not_black(img.getpixel((5, 2))):
+            if self.is_pix_not_black(img.getpixel((1, 1))):
+                if self.is_pix_not_black(img.getpixel((7, 1))):
+                    number = 7
+                else:
+                    number = 5
+            elif self.is_pix_not_black(img.getpixel((1, 4))):
+                if self.is_pix_not_black(img.getpixel((7, 3))):
+                    number = 8
+                else:
+                    number = 6
+            else:
+                if self.is_pix_not_black(img.getpixel((7, 7))):
+                    number = 2
+                else:
+                    number = 9
+        else:
+            if self.is_pix_not_black(img.getpixel((5, 6))):
+                if self.is_pix_not_black(img.getpixel((7, 7))):
+                    number = 1
+                else:
+                    number = 4
+            else:
+                if self.is_pix_not_black(img.getpixel((7, 1))):
+                    number = 3
+                else:
+                    number = 0
+
+        #print("debug number:", str(number))
+        #write
+
+        return number
+
+
     def transform_frame(self, img):
         # play area
         self.extract_colours(img.crop((95, 39, 96 + 10 * 8, 40 + 20 * 8)))
@@ -112,17 +181,20 @@ class Nes_tetris:
         # next block
         self.extract_next_block(img.crop((191, 111, 191 + 33, 111 + 17)))
 
-        # score
-
         # number of lines
+        self.extract_lines(img.crop((151, 15, 151 + 25, 15 + 9)))
+
+        # score
+        self.extract_score(img.crop((191, 55, 191 + 49, 55 + 9)))
 
         # number of level
+        self.extract_level(img.crop((207, 159, 207 + 17, 159 + 9)))
 
         return self.img_leds
 
 #for debug
 if __name__ == "__main__":
-    im = Image.open("../streaming/nes2.png").convert("RGB")
+    im = Image.open("../streaming/nes.png").convert("RGB")
     gray = im.getpixel((6,6))
     game = Nes_tetris(_gray=gray)
     leds = game.transform_frame(im)
